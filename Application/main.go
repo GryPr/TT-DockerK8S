0abfe1e547677939af4e7f3f140d91d7867dc1d3
Kevin <promkev@gmail.com>
2020-10-25 00:42:19 -0400

    Removed dead code

--- Application/main.go
+++ Application/main.go
@@ -7,13 +7,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Creating an event struct
-type event struct {
-	ID          string `json:"ID"`
-	Title       string `json:"Title"`
-	Description string `json:"Description"`
-}
-
 // Home endpoint
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
